9b990c98c67c1abbf36395ff6b72e735c14d1913
RawaanKh <109039734+RawaanKh@users.noreply.github.com>
2023-02-02 14:26:35 +0300

    Add files via upload

---
+++ ResturantSW/src/main/java/com/example/restaurantsw/Repository/RestaurantRepository.java
@@ -0,0 +1,11 @@
+package com.example.restaurantsw.Repository;
+
+import com.example.restaurantsw.Model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant,Integer> {
+    List<Restaurant> findAllByBranchType(String branchType);
+}
---
+++ ResturantSW/src/main/java/com/example/restaurantsw/RestaurantSwApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantSwApplication.class, args);
+    }
+
+}
